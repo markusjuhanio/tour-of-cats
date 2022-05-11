9f446ac2fc00f5f15016e20f876ad258e21179d4
Markus Ollonqvist <markus.ollonqvist@lojotech.fi>
2022-05-11 17:24:44 +0300

    add message interface, edit cat detail

--- src/app/common/services/message.service.ts
+++ src/app/common/services/message.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,22 @@ import { Observable, of } from 'rxjs';
 
 export class MessageService {
 
-  messages: string[] = [];
+  messages: Message[] = [];
 
   add(message: string): void {
-    this.messages.push(message);
+    const randomId = Math.floor(Math.random() * 10000);
+    if (this.messages.length >= 5) {
+      this.messages.pop();
+    }
+    this.messages.unshift({id: randomId, text: message});
   };
 
   clear() {
     this.messages = [];
+  }
+
+  remove(id: number) {
+    this.messages = this.messages.filter(msg => msg.id !== id);
   }
 
   constructor() { }
--- src/app/mock-cats.ts
+++ src/app/mock-cats.ts
@@ -4,13 +4,16 @@ export const CATS: Cat[] = [
     {
         id: 1,
         name: 'Maine',
+        gender: 'female'
     },
     {
         id: 2,
-        name: 'Miuku',
+        name: 'Miina',
+        gender: 'female'
     },
     {
         id: 3,
         name: 'Väiski',
+        gender: 'male'
     }
 ]
